b8d6e8b590a0f8b89f7fa15024f65ecc1e1d8f57
Zaccharie Attias <94083271+ZaccharieAttias@users.noreply.github.com>
2025-05-19 19:06:01 +0300

    fix

---
+++ src/app/core/core/core.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-core',
+  standalone: true,
+  imports: [],
+  templateUrl: './core.component.html',
+  styleUrl: './core.component.css',
+})
+export class CoreComponent {}
