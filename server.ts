6b8dc69877599c628cd2b30d5dd1466b7f13ee82
Zaccharie Attias <94083271+ZaccharieAttias@users.noreply.github.com>
2025-05-19 18:32:20 +0300

    fix

---
+++ server.ts
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './src/app/app.config';
+import { AppComponent } from './src/app/app.component';
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
